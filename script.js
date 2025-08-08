ba95b33fa82090cd177f4a38a9656ef29dc6a4d5
codum-developer <alainmurhabazi21@gmail.com>
2025-08-08 12:07:09 +0200

    new update 2

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ ajouterAuPannierBoutons.forEach(ajouterAuPannierBouton => {
     console.log(valeurInt)
   })
 })
+
+
+  const content = document.getElementById("headerOfShop");
+  const slides = content.children;
+  let currentIndex = 0;
+  const totalSlides = slides.length;
+
+  function showNextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    content.style.transform = `translateX(-${currentIndex * 100}%)`;
+  }
+
+  // Démarrer le carrousel avec un délai de 3 secondes entre chaque transition
+  setInterval(showNextSlide, 3000); // 3 secondes
